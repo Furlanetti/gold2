38f69742fe81e903210e7fcd5ec245c87e947110
Felipe de Souza Furlanetti <felipe.furlanetti1@gmail.com>
2013-10-09 02:27:08 -0300

    mudança teste

--- app/Config/database.php
+++ app/Config/database.php
@@ -58,7 +58,7 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.0.0.1',
+		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
 		'database' => 'site_focus',
