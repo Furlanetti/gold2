69053212a434b44eef249e13feae9ea3a110770b
Felipe de Souza Furlanetti <felipe.furlanetti1@gmail.com>
2013-10-10 18:03:37 -0300

    contactsController

--- app/Controller/ContactsController.php
+++ app/Controller/ContactsController.php
@@ -4,155 +4,65 @@ class ContactsController extends AppController {
 
     public $name = 'Contacts';
 
-    public $uses = array('ContactQuestions','ContactHi','ContactBusiness','Email', 'Company');
+    //var $helpers = array('CakePtbr.Formatacao');
 
-    /* Other vars */
+    public $uses = array('Contact');
 
-    public $page_title = '{title} - Contatos | ';
-
-    public function business(){ 
-
-
-        $this->set('layout', 'custom');
-
-    	$title = 'Fale sobre o seu negócio';
-    	$this->set('title', $title);
-        $this->set('page_title', str_replace('{title}', $title, $this->page_title)); 
-        $this->set('page_color', 'light'); 
-    	
-        //Set do menu
-        $this->set('page', 'business');
-
-        if ($this->request->is('post')) {
-
-             if(!$this->postValidation($this->request->clientIp())) {
-                $this->Session->setFlash('Você enviou um e-mail muito recentemente, aguarde pelo menos 30 segundos para enviar novamente.', 'flash_error');
-                $this-$this->redirect(array('controller'=>'contacts', 'action' => 'business'));
-            }
-
-            $data = $this->request->data;
-            $data['ContactBusiness']['ip'] = $this->request->clientIp();
-            if($this->ContactBusiness->saveAll($data)){            
-                $vars['data'] = $data['ContactBusiness'];
-                $vars['files'] = $data['ContactBusinessFiles'];
-
-                //Envia e-mail para o cliente.
-                $this->Email->send(null, $vars['data']['email'], $title, $vars, 'contact_thanks');
-
-                //Envia e-mail para o contato configurado. Manter o e-mail null para enviar para o e-mail padrão.
-                $this->Email->send(null, null, $title, $vars, 'contact_business');
-
-                $this->Session->setFlash('Contato enviado com sucesso!', 'flash_success');
-                $this->redirect(array('controller'=>'contacts', 'action' => 'business'));
-            }
-
-        }
-
-    }
-
-    public function questions(){
-
-        $this->set('layout', 'custom');
-
-    	$title = 'Alguma Pergunta?';
-    	$this->set('title', $title);
-    	$this->set('page_title', str_replace('{title}', $title, $this->page_title));  
-        $this->set('page_color', 'light'); 
-
-        //Set do menu
-        $this->set('page', 'questions');
-
-        $this->Company->recursive = -1;
-        $companies = $this->Company->find('all');
-        $this->set('companies', $companies);
-        $this->set('company', array('slug' => 'grupo-focusnetworks'));          
-
-        if ($this->request->is('post')) {
-
-            if(!$this->postValidation($this->request->clientIp())) {
-                $this->Session->setFlash('Você enviou um e-mail muito recentemente, aguarde pelo menos 30 segundos para enviar novamente.', 'flash_error');
-                $this-$this->redirect(array('controller'=>'contacts', 'action' => 'questions'));
-            }
-
-            $data = $this->request->data;
-            $data['ContactQuestions']['ip'] = $this->request->clientIp();
-            if($this->ContactQuestions->save($data)){                
-                $vars['data'] = $this->request->data['ContactQuestions'];
-
-                //Envia e-mail para o cliente.
-                $this->Email->send(null, $vars['data']['email'], $title, $vars, 'contact_thanks');
-
-                //Envia e-mail para o contato configurado. Manter o e-mail null para enviar para o e-mail padrão.
-                $this->Email->send(null, null, $title, $vars, 'contact_questions');
-
-                $this->Session->setFlash('Contato enviado com sucesso!', 'flash_success');
-                $this->redirect(array('controller'=>'contacts', 'action' => 'questions'));
-            }
-        }
-
-    }
-
-    public function hi(){
-
-        $this->set('layout', 'custom');
-
-        //Set do menu
-        $this->set('page', 'hi');
-
-    	$title = 'Diga um Oi';
-    	$this->set('title', $title);
-    	$this->set('page_title', str_replace('{title}', $title, $this->page_title));   
-        $this->set('page_color', 'light');   
-
-        $this->Company->recursive = -1;
-        $companies = $this->Company->find('all');
-        $this->set('companies', $companies);
-        $this->set('company', array('slug' => 'grupo-focusnetworks'));
-
-        if ($this->request->is('post')) {
-            if(!$this->postValidation($this->request->clientIp())) {
-                $this->Session->setFlash('Você enviou um e-mail muito recentemente, aguarde pelo menos 30 segundos para enviar novamente.', 'flash_error');
-                $this-$this->redirect(array('controller'=>'contacts', 'action' => 'hi'));
-            }
-
-            $data = $this->request->data;
-            $data['ContactHi']['ip'] = $this->request->clientIp();
-            if($this->ContactHi->save($data)){     
-                $vars['data'] = $this->request->data['ContactHi'];
-
-                //Envia e-mail para o cliente.
-                $this->Email->send(null, $vars['data']['email'], $title, $vars, 'contact_thanks');
-
-                //Envia e-mail para o contato configurado. Manter o e-mail null para enviar para o e-mail padrão.
-                $this->Email->send(null, null, $title, $vars, 'contact_hi');
-
-                $this->Session->setFlash('Contato enviado com sucesso!', 'flash_success');
-                $this->redirect(array('controller'=>'contacts', 'action' => 'hi'));
-            }
-        } 
-    }
-
-    function postValidation($ip){
-
-        $recent = $this->ContactQuestions->find('first', array('conditions' => array('ContactQuestions.ip' => $ip), 'order' => 'ContactQuestions.created DESC'));              
-
-        $date = strtotime(date('Y-m-d H:i:s'));
-
-        $date_db = strtotime($recent['ContactQuestions']['created']);
-
-        if($recent){
-            $diff = ($date - $date_db);        
-            if($diff > 30){                
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return true;
-        }
+    public function admin_index(){
+ 		$contacts = $this->Contact->find('all');
+        $this->set(compact('contacts'));
         
-    }
-    
+    	$this->set('breadcrumb', $this->breadcrumb);      
 
+    }
+
+    public function admin_new(){
+
+    	if ($this->request->is('post')) {
+            $this->Contact->create();
+            if ($this->Contact->saveAll($this->request->data)) {
+               $this->Session->setFlash('A Mensagem de contato foi cadastrada com sucesso!', 'flash_success');
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('A Mensagem de contato não pôde ser salva. Por favor, verifique os campos digitados.', 'flash_error');
+           }
+        }
+    }
+
+    public function admin_edit($id = null) {
+        
+        $this->Contact->id = $id;
+        if (!$this->Contact->exists()) {
+            throw new NotFoundException('Usuário inválido', 'flash_error');
+        }
+        if ($this->request->is('post') || $this->request->is('put')) {
+            if ($this->Contact->save($this->request->data)) {
+                $this->Session->setFlash('A Mensagem de contato foi alterado com sucesso!', 'flash_success');
+                $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash('A Mensagem de contato não pôde ser salva. Por favor, verifique os campos digitados.', 'flash_error');
+            }
+        } else {
+            $this->request->data = $this->Contact->read(null, $id);            
+        }
+    }
+
+
+    public function admin_delete($id = null) {
+
+        $this->Contact->id = $id;
+        if (!$this->Contact->exists()) {
+             $this->Session->setFlash('Esta mensagem de contato não existe!', 'flash_error');
+                $this->redirect(array('action' => 'index'));
+        }
+        if ($this->Contact->delete()) {
+            $brand = $this->Contact->findById($id);
+            $this->Session->setFlash('Mensagem de contato excluído com sucesso!', 'flash_success');
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash('Não foi possível excluir este Gold Club!', 'flash_error');
+        $this->redirect(array('action' => 'index'));
+    } 
 
 }
+?>
